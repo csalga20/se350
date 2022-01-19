888665f3157b5335fb449a1081520366038b6502
Charlie Salgado <csalga20@depaul.edu>
2022-01-18 18:03:34 -0600

    done

--- main/java/Classes/Airline.java
+++ main/java/Classes/Airline.java
@@ -1,4 +1,52 @@
 package main.java.Classes;
 
+import main.java.Exceptions.BadParameterException;
+import main.java.Exceptions.NullParameterException;
+
+import java.util.Objects;
+
 public class Airline {
+    private String airlineName;
+
+    public Airline(String name) throws BadParameterException, NullParameterException
+    {
+        setAirlineName(name);
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public void setAirlineName(String name) throws BadParameterException, NullParameterException
+    {
+        if (name == null)
+        {
+            throw new NullParameterException("Please enter an airline.");
+        }
+        if (name.length() > 8)
+        {
+            throw new BadParameterException("Airline name is too long. Must be less than 8 characters. ");
+        }
+        airlineName = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Airline airline = (Airline) o;
+        return airlineName.equals(airline.airlineName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(airlineName);
+    }
+
+    @Override
+    public String toString() {
+        return "Airline{" +
+                "airlineName='" + airlineName + '\'' +
+                '}';
+    }
 }
--- main/java/Classes/Flight.java
+++ main/java/Classes/Flight.java
@@ -1,4 +1,101 @@
 package main.java.Classes;
 
+import main.java.Exceptions.BadParameterException;
+import main.java.Exceptions.NullParameterException;
+
+import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
+
 public class Flight {
+    private Airline airline;
+    private Airport airportStart;
+    private Airport airportEnd;
+    private String flightNumber;
+    private Date departureTime;
+
+    public Flight(String line, String airStart, String airEnd, Date date)
+        throws BadParameterException, NullParameterException
+    {
+        setAirline(line);
+        setAirportStart(airStart);
+        setAirportEnd(airEnd);
+        setFlightNumber();
+        setDepartureTime(date);
+    }
+
+    public void setAirline(String line) throws BadParameterException, NullParameterException
+    {
+        airline = new Airline(line);
+    }
+
+    public void setAirportStart(String airStart) throws BadParameterException, NullParameterException
+    {
+        airportStart = new Airport(airStart);
+    }
+
+    public void setAirportEnd(String airEnd) throws BadParameterException, NullParameterException
+    {
+        airportEnd = new Airport(airEnd);
+    }
+
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber() throws BadParameterException, NullParameterException
+    {
+        flightNumber = String.valueOf(UUID.randomUUID());
+        if (flightNumber == null)
+        {
+            throw new NullParameterException("No flight number generated");
+        }
+        if (flightNumber.length() < 0)
+        {
+            throw new BadParameterException("No flight number generated.");
+        }
+    }
+
+    public Date getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(Date date) throws BadParameterException, NullParameterException
+    {
+        Date when = new Date(2022, 01, 19);
+        if (date == null)
+        {
+            throw new NullParameterException("Please enter a date for the flight");
+        }
+        if (date.before(when) == true)
+        {
+            throw new BadParameterException("Please enter a valid date after the submission date. (2022-01-19");
+        }
+        departureTime = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Flight flight = (Flight) o;
+        return airline.equals(flight.airline) && airportStart.equals(flight.airportStart) && airportEnd.equals(flight.airportEnd) && flightNumber.equals(flight.flightNumber) && departureTime.equals(flight.departureTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(airline, airportStart, airportEnd, flightNumber, departureTime);
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "airline=" + airline +
+                ", airportStart=" + airportStart +
+                ", airportEnd=" + airportEnd +
+                ", flightNumber='" + flightNumber + '\'' +
+                ", departureTime=" + departureTime +
+                '}';
+    }
 }
